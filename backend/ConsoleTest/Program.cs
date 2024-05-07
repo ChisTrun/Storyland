423bf264dc243d7cc4e149b6cf7ccde67dc6b1bb
Antinous Ngo <quocankg2611@gmail.com>
2024-05-07 20:00:36 +0700

    chore: Delete backend/ConsoleTest directory

--- backend/ConsoleTest/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConsoleTest;
-using System.Text;
-using TangThuVienPlugin;
-using TruyenFullPlugin;
-
-Console.OutputEncoding = Encoding.Unicode;
-
-var ttv = new TangThuVienCrawler();
-ttv.GetChapterContent("https://truyen.tangthuvien.vn/doc-truyen/ta-von-khong-y-thanh-tien/chuong-0");
