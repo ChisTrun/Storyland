0caff758a4f9ec09c940c854bba1f9762419994d
21120190 <quocankg2611@gmail.com>
2024-06-10 12:49:31 +0700

    refractoring: delete projects

--- backend/PluginBase/Models/ExportType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PluginBase.Models
-{
-    public class ExportType(string id, string name)
-    {
-        public string ID { get; set; } = id ;
-        public string Name { get; set; } = name;
-    }
-}
